dba7517de790a67ddaa27ad7b9411ce42b978d40
userwt <2793338797@qq.com>
2020-12-25 15:00:05 +0800

    14：59

--- src/com/yidu/bean/Main.java
+++ src/com/yidu/bean/Main.java
@@ -3,6 +3,7 @@ package com.yidu.bean;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("一度教育111");
+
+        System.out.println("一度教育111努力做到更好！");
     }
 }
